a03d62a2db337fbecf5799f94aea78755d0ecf4d
Dai <daidongly@gmail.com>
2019-01-16 15:28:05 -0500

    update phc.py

--- gym-code/gym/envs/scheduler/hpc.py
+++ gym-code/gym/envs/scheduler/hpc.py
@@ -10,6 +10,9 @@ from gym.envs.scheduler.job import Workloads
 from gym.envs.scheduler.cluster import Cluster
 from gym.envs.scheduler.cluster import Machine
 
+# HPC Batch Scheduler Simulation.
+# 
+# Created by Dong Dai. Licensed on the same terms as the rest of OpenAI Gym.
 
 max_queue_size = 50
 empty_job_str = "0        0      0    0   0     0    0   0  0 0  0   0   0  0  0 0 0 0"
@@ -24,14 +27,32 @@ job_feature_size = 3
 
 np.random.seed(1)
 
-# HPC Batch Scheduler Simulation.
-# 
-# Created by Dong Dai. Licensed on the same terms as the rest of OpenAI Gym.
 
 class HpcEnv(gym.Env):
+
+    """The main HPC Environment class. 
+    The main API methods that users of this class need to know are:
+
+        step
+        reset
+        render
+        close
+        seed
+
+    And set the following attributes:
+
+        action_space: The Space object corresponding to valid actions
+        observation_space: The Space object corresponding to valid observations
+        reward_range: A tuple corresponding to the min and max possible rewards
+
+    Note: a default reward range set to [-inf,+inf] already exists. Set it if you want a narrower range.
+
+    The methods are accessed publicly as "step", "reset", etc.. The
+    non-underscored versions are wrapper methods to which we may add
+    functionality over time.
+    """
+
     def __init__(self, workload_file = ''):
-        # workload_file = "./data/RICC-2010-2.swf"
-        print ("current working dir: ", os.getcwd())
         print ("loading workloads from dataset:", workload_file)
         super(HpcEnv, self).__init__()
         self.workload_file = workload_file
@@ -53,22 +74,100 @@ class HpcEnv(gym.Env):
 
         self.n_actions = max_queue_size
         self.n_features = max_queue_size * job_feature_size
+    
+    def render(self):
+        """Renders the environment.
+
+        The set of supported modes varies per environment. (And some
+        environments do not support rendering at all.) By convention,
+        if mode is:
+
+        - human: render to the current display or terminal and
+          return nothing. Usually for human consumption.
+        - rgb_array: Return an numpy.ndarray with shape (x, y, 3),
+          representing RGB values for an x-by-y pixel image, suitable
+          for turning into a video.
+        - ansi: Return a string (str) or StringIO.StringIO containing a
+          terminal-style text representation. The text can include newlines
+          and ANSI escape sequences (e.g. for colors).
+
+        Note:
+            Make sure that your class's metadata 'render.modes' key includes
+              the list of supported modes. It's recommended to call super()
+              in implementations to use the functionality of this method.
+
+        Args:
+            mode (str): the mode to render with
+            close (bool): close all open renderings
+
+        Example:
+
+        class MyEnv(Env):
+            metadata = {'render.modes': ['human', 'rgb_array']}
+
+            def render(self, mode='human'):
+                if mode == 'rgb_array':
+                    return np.array(...) # return RGB frame suitable for video
+                elif mode is 'human':
+                    ... # pop up a window and render
+                else:
+                    super(MyEnv, self).render(mode=mode) # just raise an exception
+        """
+        return 0
+
+    def close(self):
+        """Override _close in your subclass to perform any necessary cleanup.
+
+        Environments will automatically close() themselves when
+        garbage collected or when the program exits.
+        """
+        return 0
 
     def seed(self, seed=None):
+        """Sets the seed for this env's random number generator(s).
+
+        Note:
+            Some environments use multiple pseudorandom number generators.
+            We want to capture all such seeds used in order to ensure that
+            there aren't accidental correlations between multiple generators.
+
+        Returns:
+            list<bigint>: Returns the list of seeds used in this env's random
+              number generators. The first value in the list should be the
+              "main" seed, or the value which a reproducer should pass to
+              'seed'. Often, the main seed equals the provided 'seed', but
+              this won't be true if seed=None, for example.
+        """
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
     def step(self, action):
+        """Run one timestep of the environment's dynamics. When end of
+        episode is reached, you are responsible for calling `reset()`
+        to reset this environment's state.
+
+        Accepts an action and returns a tuple (observation, reward, done, info).
+
+        Args:
+            action (object): an action provided by the environment
+
+        Returns:
+            observation (object): agent's observation of the current environment
+            reward (float) : amount of reward returned after previous action
+            done (boolean): whether the episode has ended, in which case further step() calls will return undefined results
+            info (dict): contains auxiliary diagnostic information (helpful for debugging, and sometimes learning)
+        """
         return 0
 
     def reset(self):
+        """Resets the state of the environment and returns an initial observation.
+
+        Returns: observation (object): the initial observation of the
+            space.
+        """
         self.cluster.reset()
         self.random_start()
         return 0
-
-
-
-
 
     def build_queue_vector(self):
         vector = np.array([])
