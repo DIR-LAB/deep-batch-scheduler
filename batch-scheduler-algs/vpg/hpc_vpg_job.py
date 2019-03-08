adce203b314974aaf9d1f2905473ca4243862425
Dai <daidongly@gmail.com>
2019-03-07 20:29:30 -0500

    update with cnn

--- batch-scheduler-algs/vpg/hpc_vpg_job.py
+++ batch-scheduler-algs/vpg/hpc_vpg_job.py
@@ -587,7 +587,7 @@ if __name__ == '__main__':
     parser.add_argument('--cpu', type=int, default=1)
     parser.add_argument('--steps', type=int, default=4000)
     parser.add_argument('--epochs', type=int, default=2000)
-    parser.add_argument('--exp_name', type=str, default='hpc-cnn-1')
+    parser.add_argument('--exp_name', type=str, default='hpc-cnn-2')
     args = parser.parse_args()
 
     mpi_fork(args.cpu)  # run parallel code with mpi
