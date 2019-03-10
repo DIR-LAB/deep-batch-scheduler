b7b624eb10619b19c729477a907a9688877e38c1
Dai <daidongly@gmail.com>
2019-03-10 11:12:40 -0400

    update learning rate

--- batch-scheduler-algs/vpg/hpc_vpg_job.py
+++ batch-scheduler-algs/vpg/hpc_vpg_job.py
@@ -597,7 +597,7 @@ if __name__ == '__main__':
     parser.add_argument('--gamma', type=float, default=1.0)
     parser.add_argument('--seed', '-s', type=int, default=0)
     parser.add_argument('--cpu', type=int, default=1)
-    parser.add_argument('--steps', type=int, default=6400)
+    parser.add_argument('--steps', type=int, default=2000)
     parser.add_argument('--epochs', type=int, default=2000)
     parser.add_argument('--exp_name', type=str, default='hpc-basic-cnn')
     args = parser.parse_args()
