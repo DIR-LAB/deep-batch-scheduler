a26dd8489ff84dbc9ff5e90359b78591656411fe
Dai <daidongly@gmail.com>
2019-03-15 10:19:12 -0400

    update ppo code

--- batch-scheduler-algs/ppo/hpc_ppo.py
+++ batch-scheduler-algs/ppo/hpc_ppo.py
@@ -523,7 +523,7 @@ if __name__ == '__main__':
     parser.add_argument('--seed', '-s', type=int, default=0)
     parser.add_argument('--steps', type=int, default=6400)
     parser.add_argument('--epochs', type=int, default=6000)
-    parser.add_argument('--exp_name', type=str, default='hpc-ppo-6400')
+    parser.add_argument('--exp_name', type=str, default='hpc-ppo-6400-unsorted')
     args = parser.parse_args()
 
     mpi_fork(1)  # run parallel code with mpi
