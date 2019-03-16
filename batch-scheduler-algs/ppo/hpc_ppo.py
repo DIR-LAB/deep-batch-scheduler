835bffb42557561d803609621b08f0170ff3eb09
Dai <daidongly@gmail.com>
2019-03-16 08:12:29 -0400

    update without pretrain

--- batch-scheduler-algs/ppo/hpc_ppo.py
+++ batch-scheduler-algs/ppo/hpc_ppo.py
@@ -230,7 +230,7 @@ with early stopping based on approximate KL
 
 def ppo(env_name, workload_file, model_path, ac_kwargs=dict(), seed=0,
         steps_per_epoch=4000, epochs=50, gamma=0.99, clip_ratio=0.2, pi_lr=3e-2,
-        vf_lr=1e-2, train_pi_iters=60, train_v_iters=80, lam=0.97, max_ep_len=1000,
+        vf_lr=1e-2, train_pi_iters=60, train_v_iters=80, lam=0.97, max_ep_len=10000,
         target_kl=0.01, logger_kwargs=dict(), save_freq=10):
     """
 
