96ed3b081b0d41a1b0c02e6e9a7340a316b81edb
Dai <daidongly@gmail.com>
2019-03-17 22:26:11 -0400

    update reward to direct optimize bsld. ppo direct and env_direct

--- gym-hpc-scheduler/hpc/envs/env_simple_direct.py
+++ gym-hpc-scheduler/hpc/envs/env_simple_direct.py
@@ -89,8 +89,8 @@ class SimpleDirectHPCEnv(gym.Env):
         # random.randint(MAX_JOBS_EACH_BATCH, (self.loads.size() - 2 * MAX_JOBS_EACH_BATCH))
         # make sure we restart from the begining again. Always sample at the beginning.
         self.start = self.deterministic_index + MAX_JOBS_EACH_BATCH
-        self.deterministic_index = (self.deterministic_index + 1) % (self.loads.size() - MAX_JOBS_EACH_BATCH)
-        print("start:", self.start)
+        self.deterministic_index = (self.deterministic_index + 1) % (self.loads.size() - 2 * MAX_JOBS_EACH_BATCH)
+        # print("start:", self.start)
 
         self.num_job_in_batch = random.randint(MAX_JOBS_EACH_BATCH, MAX_JOBS_EACH_BATCH)
         self.last_job_in_batch = self.start + self.num_job_in_batch
