b40679def6e6d4dd4b6f8c0ca4b4d42dbb9ab8f0
Dai <daidongly@gmail.com>
2019-03-22 17:58:12 -0400

    try cnn in hpc_ppo_simple.py

--- batch-scheduler-algs/ppo/hpc_ppo_simple.py
+++ batch-scheduler-algs/ppo/hpc_ppo_simple.py
@@ -16,6 +16,7 @@ from spinup.utils.mpi_tools import mpi_fork, mpi_avg, proc_id, mpi_statistics_sc
 EPS = 1e-8
 
 MAX_QUEUE_SIZE = 35
+MAX_MACHINE_SIZE = 256
 MLP_SIZE = 256
 
 # each job has three features: submit_time, request_number_of_processors, request_time/run_time,
@@ -47,6 +48,52 @@ def mlp(x, hidden_sizes=(32,), activation=tf.tanh, output_activation=None):
         x = tf.layers.dense(x, units=h, activation=activation)
     return tf.layers.dense(x, units=hidden_sizes[-1], activation=output_activation)
 
+
+# Basic CNN Architecture
+def basic_cnn(x_ph, act_dim):
+    sq = int(math.ceil(math.sqrt(MAX_QUEUE_SIZE)))
+    x = tf.reshape(x_ph, shape=[-1, sq, sq, JOB_FEATURES])
+    conv1 = tf.layers.conv2d(
+            inputs=x,
+            filters=32,
+            kernel_size=[3, 3],
+            strides=1,
+            padding='same',
+            activation=tf.nn.relu
+    )
+    pool1 = tf.layers.max_pooling2d(
+            inputs=conv1,
+            pool_size=[2, 2],
+            strides=2
+    )
+    conv2 = tf.layers.conv2d(
+            inputs=pool1,
+            filters=64,
+            kernel_size=[3, 3],
+            strides=1,
+            padding='same',
+            activation=tf.nn.relu
+    )
+    pool2 = tf.layers.max_pooling2d(
+            inputs=conv2,
+            pool_size=[2, 2],
+            strides=2
+    )
+    flat = tf.reshape(pool2, [-1, 64])
+    dense = tf.layers.dense(
+            inputs=flat,
+            units=1024,
+            activation=tf.nn.relu
+    )
+    dropout = tf.layers.dropout(
+            inputs=dense,
+            rate=0.5,
+    )
+    return tf.layers.dense(
+            inputs=dropout,
+            units=act_dim
+    )
+
 def get_vars(scope=''):
     return [x for x in tf.trainable_variables() if scope in x.name]
 
@@ -63,7 +110,8 @@ Policies
 """
 def categorical_policy(x, a, action_space):
     act_dim = action_space.n
-    logits = mlp(x, list((MLP_SIZE,MLP_SIZE,MLP_SIZE,MLP_SIZE))+[act_dim], tf.tanh, None)
+    # logits = mlp(x, list((MLP_SIZE,MLP_SIZE,MLP_SIZE,MLP_SIZE))+[act_dim], tf.tanh, None)
+    logits = basic_cnn(x, act_dim)
     logp_all = tf.nn.log_softmax(logits)
     pi = tf.squeeze(tf.multinomial(logits,1), axis=1)
     logp = tf.reduce_sum(tf.one_hot(a, depth=act_dim) * logp_all, axis=1)
@@ -78,7 +126,8 @@ def actor_critic(x, a, action_space=None):
     with tf.variable_scope('pi'):
         logits, pi, logp, logp_pi = categorical_policy(x, a, action_space)
     with tf.variable_scope('v'):
-        v = tf.squeeze(mlp(x, list((MLP_SIZE,MLP_SIZE,MLP_SIZE,MLP_SIZE))+[1], tf.tanh, None), axis=1)
+        # v = tf.squeeze(mlp(x, list((MLP_SIZE,MLP_SIZE,MLP_SIZE,MLP_SIZE))+[1], tf.tanh, None), axis=1)
+        v = tf.squeeze(basic_cnn(x, 1), axis=1)
     return logits, pi, logp, logp_pi, v
 
 
