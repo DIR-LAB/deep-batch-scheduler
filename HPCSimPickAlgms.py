250b6596fc88942c61ef3a8489598545a1e50024
Dong Dai <daidongly@gmail.com>
2019-08-23 18:33:36 -0400

    what about rwd

--- HPCSimPickAlgms.py
+++ HPCSimPickAlgms.py
@@ -470,15 +470,15 @@ class HPCEnv(gym.Env):
         else:
             rl_total = sum(self.scheduled_rl.values())
             best_total = min(self.scheduled_scores) 
-            # rwd = (best_total - rl_total)
-            
+            rwd = (best_total - rl_total)
+            '''
             if (best_total) < rl_total:
                 rwd = -1
             elif best_total > (rl_total):
-                rwd = 5
+                rwd = 1
             else:
                 rwd = 0
-            
+            '''
             return [None, rwd, True, None]
     
     def step_for_test(self, a):
