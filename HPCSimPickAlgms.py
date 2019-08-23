9b2f9b8c33dc84129c4b6712ac8125dfdc56b0a7
Dong Dai <daidongly@gmail.com>
2019-08-23 13:26:48 -0400

    positive +5, add skip action

--- HPCSimPickAlgms.py
+++ HPCSimPickAlgms.py
@@ -29,7 +29,7 @@ JOB_FEATURES = 4
 DEBUG = False
 
 # we have a really bad performance when training with 128 job sequence. Change it to 32 and see whether it would be better
-JOB_SEQUENCE_SIZE = 32
+JOB_SEQUENCE_SIZE = 16
 ALGMS_SIZE = 4
 
 def combined_shape(length, shape=None):
