add parameters to control sjf calculation in training and testing phases. Testing phase has no SJF calculation and does not consider whether a traj is having correct score

ppo-pick-jobs.py
@@ -247,7 +247,7 @@ def ppo(workload_file, model_path, ac_kwargs=dict(), seed=0,
     tf.set_random_seed(seed)
     np.random.seed(seed)
 
-    env = HPCEnv(shuffle=shuffle, backfil=backfil, skip=skip, job_score_type=score_type, batch_job_slice=batch_job_slice)
+    env = HPCEnv(shuffle=shuffle, backfil=backfil, skip=skip, job_score_type=score_type, batch_job_slice=batch_job_slice, build_sjf=True)
     env.seed(seed)
     env.my_init(workload_file=workload_file, sched_file=model_path)
     